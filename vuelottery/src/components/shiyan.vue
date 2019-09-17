57d9a6b9fe5a38cd2d80d9248b3c5c7b26c0f488
cxywll <1513856399@qq.com>
2019-09-17 20:48:40 +0800

    实验

---
+++ vuelottery/src/components/shiyan.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>asdshdjmsnds</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    div{
+        font-size: 0.3rem;
+    }
+</style>
---
+++ vuelottery/src/views/shiyan.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>232523</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    div{
+        font-size: 0.3rem;
+    }
+</style>
