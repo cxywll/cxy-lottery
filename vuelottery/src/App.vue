a1034ac2f5738fdbceab2c3f7c86120983d70c39
z-yongchao <493492875@qq.com>
2019-09-18 20:19:20 +0800

    小赵

--- vuelottery/src/App.vue
+++ vuelottery/src/App.vue
@@ -5,10 +5,8 @@
 </template>
 
 <style>
-*{margin: 0;padding: 0;text-decoration: none;list-style: none;}
-<<<<<<< HEAD
-=======
-body,html{height: 100%;}
+*{margin: 0;padding: 0;text-decoration: none;list-style: none;font-style:normal}
+body,html{height: 100%;}  
 #app{
   height: 100%;
 }
@@ -24,5 +22,4 @@ body,html{height: 100%;}
     zoom: 1;
 }
 
->>>>>>> f04314b469e3a24dedc52f558bdd678e3e075e3d
 </style>
--- vuelottery/src/router.js
+++ vuelottery/src/router.js
@@ -11,20 +11,20 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-<<<<<<< HEAD
-      path: '/',
-      name: 'Cfoot',
-      component: Cfoot
-=======
-      path: '/shiyan',
-      name: 'shiyan',
-      component: shiyan
->>>>>>> f04314b469e3a24dedc52f558bdd678e3e075e3d
-    },
-    {
       path: '/',
       name: 'home',
       component: Home
     },
+    {
+      path: '/foot',
+      name: 'Cfoot',
+      component: Cfoot
+    }, 
+    {
+      path: '/shiyan',
+      name: 'shiyan',
+      component: shiyan
+    },
+    
   ]
 })
