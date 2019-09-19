3ffd25bfe2db04bd3e0e92237110459d9f428f69
Your Name <you@example.com>
2019-09-19 14:58:14 +0800

    wcj

--- vuelottery/src/router.js
+++ vuelottery/src/router.js
@@ -3,7 +3,8 @@ import Router from 'vue-router'
 import shiyan from './views/shiyan.vue'
 import Home from './views/z-home.vue'
 import Cfoot from './components/C-foot.vue'
-
+import touzhu from './views/j-touzhu.vue'
+import alert from '@/components/alert.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -19,6 +20,16 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/touzhu',
+      name: 'touzhu',
+      component: touzhu
+    },
+    {
+      path: '/alert',
+      name: 'alert',
+      component: alert
     },
   ]
 })
