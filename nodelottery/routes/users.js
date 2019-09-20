336a2eac8973088b92e63b10adb43f1a76fc1040
z-yongchao <493492875@qq.com>
2019-09-20 11:13:45 +0800

    ...

--- nodelottery/routes/users.js
+++ nodelottery/routes/users.js
@@ -13,7 +13,7 @@ router.post('/up', function (req, res, next) {
     })
   } else {
     var user_status = true
-    var text = eval(fs.readFileSync('login.txt', 'utf8'))
+    var text = eval(fs.readFileSync('./login.txt', 'utf8'))
     for (var i = 0; i < text.length; i++) {
       if (text[i].user == json.user) {
         user_status = false
@@ -21,7 +21,7 @@ router.post('/up', function (req, res, next) {
     }
     if (user_status) {
       text.push(json)
-      fs.writeFileSync('login.txt',text,'utf8')
+      fs.writeFileSync('./login.txt', JSON.stringify(text), 'utf8')
       res.send({
         type: 1,
         data: '注册成功'
@@ -37,6 +37,31 @@ router.post('/up', function (req, res, next) {
 });
 //登录
 router.post('/in', function (req, res, next) {
-  res.send('respond with a resource');
+  var json = req.body;
+  if (json.user == "" || json.pass == "") {
+    res.send({
+      type: 0,
+      data: '请输入账号和密码'
+    })
+  } else {
+    var user_status = true
+    var text = eval(fs.readFileSync('./login.txt', 'utf8'))
+    for (var i = 0; i < text.length; i++) {
+      if (text[i].user == json.user && text[i].pass == json.pass) {
+          user_status = false
+      }
+    }
+    if (user_status) {
+      res.send({
+        type: 1,
+        data: '账号密码不匹配'
+      })
+    } else {
+      res.send({
+        type: 2,
+        data: text
+      })
+    }
+  }
 });
 module.exports = router;
