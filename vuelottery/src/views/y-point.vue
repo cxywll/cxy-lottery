8f6c14a8224937b7e67ad5e70547dad072584e45
cxywll <1513856399@qq.com>
2019-10-14 19:49:24 +0800

    兑换

--- vuelottery/src/views/y-point.vue
+++ vuelottery/src/views/y-point.vue
@@ -13,58 +13,58 @@
 					<a class="v-tabs__item v-tabs__item--active" style="position: static;" :class="[!type?'active':'']">购物区</a>
 				</div>
 		</div>
-	<div class="left" v-show='type'>
-		<div class="iph">
-			<wcjs class="j-btn" @click="typealter=true">
-			<img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1546077945538&di=003d3de1479ae95ab6131ca11d042c4a&imgtype=0&src=http%3A%2F%2Fa.vpimg3.com%2Fupload%2Fmerchandise%2Fpdcvis%2F146767%2F2017%2F1104%2F115%2F88257c95-7205-4390-8eab-635fc8384069_t.jpg">
-			<p class="text-xs-center mb-0">iphoneX (尊贵铂金)</p>
-			<p class="text-xs-center mb-0">积分：999</p>
-			<p class="text-xs-center mb-0"><small>市场参考价：9999.00</small></p>
-			</wcjs>
-		</div>
-		<div class="iph">
-			<wcjs class="j-btn" @click="typealter=true">
-			<div style="margin:20px;">
-				<img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1546077790483&di=716de166c55fe352462ca970809e926f&imgtype=0&src=http%3A%2F%2Fi1.hexunimg.cn%2F2016-03-15%2F182772684.jpg">
+		<div class="left" v-show='type'>
+			<div class="iph">
+				<wcjs class="j-btn" @click="typealter=true">
+					<img src="https://timgsa.baidu.com/timg?image&quality=80&		size=b9999_10000&sec=1546077945538&		di=003d3de1479ae95ab6131ca11d042c4a&imgtype=0&		src=http%3A%2F%2Fa.vpimg3.		com%2Fupload%2Fmerchandise%2Fpdcvis%2F146767%2F2017%2F1104%2F115%2F88257c95-7205-4390-8eab-635fc8384069_t.jpg">
+					<p class="text-xs-center mb-0">iphoneX (尊贵铂金)</p>
+					<p class="text-xs-center mb-0">积分：999</p>
+					<p class="text-xs-center mb-0"><small>市场参考价：9999.00</small></p>
+				</wcjs>
 			</div>
-			<p class="text-xs-center mb-0">话费充值卡</p>
-			<p class="text-xs-center mb-0">积分：100.00</p>
-			<p class="text-xs-center mb-0"><small>市场参考价：9999.00</small></p>
+		<div class="iph">
+			<wcjs class="j-btn" @click="typealter=true">
+				<div style="margin:20px;">
+					<img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1546077790483&di=716de166c55fe352462ca970809e926f&imgtype=0&src=http%3A%2F%2Fi1.hexunimg.cn%2F2016-03-15%2F182772684.jpg">
+				</div>
+				<p class="text-xs-center mb-0">话费充值卡</p>
+				<p class="text-xs-center mb-0">积分：100.00</p>
+				<p class="text-xs-center mb-0"><small>市场参考价：9999.00</small></p>
 			</wcjs>
 		</div> 
-	</div>
-	<div class="right" v-show='!type'>
-		<div class="iph"> 
-			<wcjs class="j-btn" @click="typealter=true">
-				<div style="margin:20px;">
-					<img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1546081779966&di=20ebc0440402aa12a980d968b228e5ce&imgtype=0&src=http%3A%2F%2Fimages.51bi.com%2Fopt%2Fsiteimg%2Fp%2F20140610%2F1043b5f316d16f0a00c722bec039fac5.jpeg">
-				</div>
-				<p class="text-xs-center mb-0">烟斗 (中号)</p>
-				<p class="text-xs-center mb-0">积分：9999</p>
-				<p class="text-xs-center mb-0"><small>市场参考价：9999.00</small></p>
-			</wcjs>
 		</div>
-		<div class="iph">
-			<wcjs class="j-btn" @click="typealter=true">
-				<div style="margin:20px;">
-					<img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1546081779966&di=20ebc0440402aa12a980d968b228e5ce&imgtype=0&src=http%3A%2F%2Fimages.51bi.com%2Fopt%2Fsiteimg%2Fp%2F20140610%2F1043b5f316d16f0a00c722bec039fac5.jpeg">
-				</div>
-				<p class="text-xs-center mb-0">烟斗 (中号)</p>
-				<p class="text-xs-center mb-0">积分：9999</p>
-				<p class="text-xs-center mb-0"><small>市场参考价：9999.00</small></p>
-			</wcjs>
+		<div class="right" v-show='!type'>
+			<div class="iph"> 
+				<wcjs class="j-btn" @click="typealter=true">
+					<div style="margin:20px;">
+						<img src="https://timgsa.baidu.com/timg?image&quality=80&	size=b9999_10000&sec=1546081779966&	di=20ebc0440402aa12a980d968b228e5ce&imgtype=0&	src=http%3A%2F%2Fimages.51bi.	com%2Fopt%2Fsiteimg%2Fp%2F20140610%2F1043b5f316d16f0a00c722bec	039fac5.jpeg">
+					</div>
+					<p class="text-xs-center mb-0">烟斗 (中号)</p>
+					<p class="text-xs-center mb-0">积分：9999</p>
+					<p class="text-xs-center mb-0"><small>市场参考价：9999.00</small></p>
+				</wcjs>
+			</div>
+			<div class="iph">
+				<wcjs class="j-btn" @click="typealter=true">
+					<div style="margin:20px;">
+						<img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1546081779966&di=20ebc0440402aa12a980d968b228e5ce&imgtype=0&src=http%3A%2F%2Fimages.51bi.com%2Fopt%2Fsiteimg%2Fp%2F20140610%2F1043b5f316d16f0a00c722bec039fac5.jpeg">
+					</div>
+					<p class="text-xs-center mb-0">烟斗 (中号)</p>
+					<p class="text-xs-center mb-0">积分：9999</p>
+					<p class="text-xs-center mb-0"><small>市场参考价：9999.00</small></p>
+				</wcjs>
+			</div>
+			<div class="iph">
+				<wcjs class="j-btn" @click="typealter=true">
+					<div style="margin:20px;">
+						<img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1546081779966&di=20ebc0440402aa12a980d968b228e5ce&imgtype=0&src=http%3A%2F%2Fimages.51bi.com%2Fopt%2Fsiteimg%2Fp%2F20140610%2F1043b5f316d16f0a00c722bec039fac5.jpeg">
+					</div>
+					<p class="text-xs-center mb-0">烟斗 (中号)</p>
+					<p class="text-xs-center mb-0">积分：9999</p>
+					<p class="text-xs-center mb-0"><small>市场参考价：9999.00</small></p>
+				</wcjs>
+			</div>      
 		</div>
-		<div class="iph">
-			<wcjs class="j-btn" @click="typealter=true">
-				<div style="margin:20px;">
-					<img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1546081779966&di=20ebc0440402aa12a980d968b228e5ce&imgtype=0&src=http%3A%2F%2Fimages.51bi.com%2Fopt%2Fsiteimg%2Fp%2F20140610%2F1043b5f316d16f0a00c722bec039fac5.jpeg">
-				</div>
-				<p class="text-xs-center mb-0">烟斗 (中号)</p>
-				<p class="text-xs-center mb-0">积分：9999</p>
-				<p class="text-xs-center mb-0"><small>市场参考价：9999.00</small></p>
-			</wcjs>
-		</div>      
-	</div>
 	</div>
 
 	
